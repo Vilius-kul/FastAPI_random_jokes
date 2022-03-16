bd9392d681c04ed742da98fb8013101623d037a1
vilius <vilius.kulinas@gmail.com>
2022-03-16 10:08:11 +0000

    jokes to db as background task

--- Fast_jokes/api_calls/jokes_api.py
+++ Fast_jokes/api_calls/jokes_api.py
@@ -1,22 +1,34 @@
-import fastapi
 from clients.jokeapi import JokeAPI
-from fastapi import Depends
+from fastapi import APIRouter, BackgroundTasks, Depends
 from jokes_piccoloapp.tables import JokesEnglish
 from models.userinput import UserInput
 
-router = fastapi.APIRouter()
+router = APIRouter()
+
+
+async def to_database_single(joke: str):
+    await JokesEnglish.insert(JokesEnglish(joke=joke))
+
+
+async def to_database_multi(jokes: list[str]):
+    for joke in jokes:
+        await JokesEnglish.insert(JokesEnglish(joke=joke))
 
 
 @router.get("/random-joke")
-async def random_joke():
+async def random_joke(background_tasks: BackgroundTasks):
     joke = await JokeAPI.get_random_joke()
-    await JokesEnglish.insert(
-        JokesEnglish(joke=joke)
+    # TODO: do something if api returns empty or some other non string stuff...
+    background_tasks.add_task(
+        to_database_single, joke
     )  # inserts jokes into db, JokesEnglish table
     return joke
 
 
 @router.get("/multi-jokes")
-async def multi_joke(count: UserInput = Depends()):
-    # TODO: add db insert function
-    return await JokeAPI.multiple_jokes(count.joke_count)
+async def multi_joke(background_tasks: BackgroundTasks, count: UserInput = Depends()):
+    jokes = await JokeAPI.multiple_jokes(count.joke_count)
+    background_tasks.add_task(
+        to_database_multi, jokes
+    )  # inserts jokes into db, JokesEnglish table
+    return jokes

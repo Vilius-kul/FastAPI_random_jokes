49df85b253ec140c828e461ac996502de250efda
vilius <vilius.kulinas@gmail.com>
2022-03-17 17:39:42 +0000

    Field() approach vs @validator...

--- Fast_jokes/models/userinput.py
+++ Fast_jokes/models/userinput.py
@@ -3,3 +3,14 @@ from pydantic import BaseModel, Field, ValidationError, validator
 
 class UserInput(BaseModel):
     joke_count: int = Field(gt=0, lt=11, description="Must be between 1 and 10")
+
+
+# couldn't make it work properly
+# class UserInput(BaseModel):
+#     joke_count: int
+
+#     @validator("joke_count")
+#     def limit_count(cls, v):
+#         if v not in range(1, 11):
+#             raise ValueError("Out of range! Try 1 to 10.")
+#         return v
